9c3cbc01c42e42d5b1a8466448fd6f2ca2b85af9
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-09-06 18:32:16 +0530

    Analytics and sticky header

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,7 +10,6 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark;
   const siteTitle = data.site.siteMetadata.title;
   const { previous, next } = pageContext;
-  console.log("post.frontmatter", post.frontmatter);
 
   return (
     <Layout location={location} title={siteTitle}>
