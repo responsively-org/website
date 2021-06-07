b828fe105935f7dea09aea4a0620744eb012df62
jjavierdguezas <jjavierdguezas@gmail.com>
2021-06-07 14:57:54 +0200

    ensure that only one carbon ad is visible
    - remove extra ads (starts with `carbonads_` as done in
    https://github.com/tannerlinsley/react-query/blob/master/docs/src/components/CarbonAds.js

--- src/components/CarbonAds/index.js
+++ src/components/CarbonAds/index.js
@@ -23,6 +23,18 @@ export const CarbonAds = React.memo(() => {
     return () => clearTimeout(timeout);
   }, [ref]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      [...ref.current.children].forEach((child) => {
+        if (child.id.startsWith("carbonads_")) {
+          ref.current.removeChild(child);
+        }
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  });
+
   return (
     <div
       ref={ref}
