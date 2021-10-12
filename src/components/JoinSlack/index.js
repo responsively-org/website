0066df808e62b4b66da81dba2baa029ab83aad94
Manoj Vivek <p.manoj.vivek@gmail.com>
2021-10-12 13:09:55 +0530

    Sponsor US floating button

--- src/components/JoinSlack/index.js
+++ src/components/JoinSlack/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-import image from "./image.svg";
+import image from "./raised-fist-emoji.png";
 import "./style.scss";
 
 const JoinSlack = () => (
-  <a href="/join-slack" className="JoinSlack" target="_blank" rel="noreferrer">
-    <img src={image} alt="Join us on slack" />
+  <a
+    href="https://opencollective.com/responsively"
+    className="JoinSlack"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <img src={image} alt="Sponsor us" height={30} /> Sponsor us!
   </a>
 );
 
