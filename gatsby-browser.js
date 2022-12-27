f3da6d23669ff7f1107594ecae974c331285fe7d
Manoj Vivek <p.manoj.vivek@gmail.com>
2022-12-27 18:06:59 +0530

    Replaced script tag with js loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,8 @@ import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.css";
+
+const scriptTag = document.createElement("script");
+scriptTag.src = "https://analytics.responsively.app/tracker.js";
+scriptTag.defer = true;
+document.body.appendChild(scriptTag);
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "gatsby";
 import Sticky from "sticky-js";
-import { Helmet } from "react-helmet";
 
 const Header = () => {
   const navRef = useRef();
@@ -17,10 +16,6 @@ const Header = () => {
 
   return (
     <div className="navbar-container bg-primary-3">
-      <Helmet>
-        <script defer src="https://analytics.responsively.app/tracker.js" />
-      </Helmet>
-
       <nav
         ref={navRef}
         className="navbar navbar-expand-lg navbar sticky-top navbar-dark bg-primary-3"
