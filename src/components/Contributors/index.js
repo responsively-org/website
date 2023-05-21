9696ba8b65d7923d22179fc9dfc9adf4dbd36beb
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-05-21 12:54:30 +0530

    Added all sponsors to the website

--- src/components/Contributors/index.js
+++ src/components/Contributors/index.js
@@ -23,6 +23,7 @@ const fixContributors = (list) => {
 
 const Contributors = () => {
   const [contributors, setContributors] = useState([]);
+  const [allSponsors, setAllSponsors] = useState([]);
   useEffect(() => {
     (async () => {
       const contributors = JSON.parse(
@@ -34,20 +35,89 @@ const Contributors = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    // Get Sponsors from GitHub API and Open Collective API and merge them and sort them by amount donated (descending)
+    // and then by name (ascending) and then set the state allSponsors.
+    (async () => {
+      const sponsors = await fetch(
+        "https://opencollective.com/responsively/members.json"
+      ).then((response) => response.json());
+
+      console.log("sponosrs", sponsors);
+
+      const allSponsors = sponsors
+        .flat()
+        .filter(
+          (sponsor) =>
+            sponsor.role === "BACKER" &&
+            sponsor.profile !== "https://opencollective.com/bairesdev" &&
+            sponsor.name !== "Guest"
+        )
+        .map((sponsor) => ({
+          profile: sponsor.profile,
+          name: sponsor.name,
+          image:
+            sponsor.image ??
+            `${sponsor.profile.replace(
+              "https://",
+              "https://images."
+            )}/avatar.png`,
+          amount: sponsor.totalAmountDonated,
+          lastDonation: new Date(sponsor.lastTransactionAt).getTime(),
+        }))
+        .sort((a, b) => {
+          if (a.lastDonation !== b.lastDonation) {
+            return b.lastDonation - a.lastDonation;
+          }
+          if (a.amount !== b.amount) {
+            return b.amount - a.amount;
+          }
+          return a.name.localeCompare(b.name);
+        });
+
+        allSponsors.push(
+          {
+            image: "https://avatars.githubusercontent.com/u/11783457?v=4",
+            name: "Daniel Einars",
+            profile: "https://github.com/polaroidkidd",
+            amount: 10,
+            lastDonation: new Date("2023-03-01").getTime(),
+          },
+          {
+            image: "https://avatars.githubusercontent.com/u/25272108?v=4",
+            name: "Trista Lanette Pollard",
+            profile: "https://github.com/tlanettepollard",
+            amount: 50,
+            lastDonation: new Date("2023-02-12").getTime(),
+          }
+        );
+      console.log("allSponsors", allSponsors);
+      setAllSponsors(allSponsors);
+    })();
+  }, []);
+
   if (!contributors.length) {
     return null;
   }
 
   return (
-    <section>
+    <section className="container">
       <div className="justify-content-center text-center">
         <h3 className="github-contributors__thanks">
-          Our Gold Sponsors!&nbsp;
+          Our Gold Sponsors&nbsp;
           <span role="img" aria-label="appreciation">
             🤝
           </span>
         </h3>
         <div className="github-contributors__sponsors">
+          <a
+            className="github-contributors__sponsors__logo github-contributors__sponsors__logo-placeholder"
+            target="__blank"
+            rel="noreferrer"
+            href="https://accelerator.github.com/#:~:text=responsively%2Dorg/responsively%2Dapp"
+          >
+            <img src="/assets/img/logos/github.png" alt="GitHub" width="264" />
+          </a>
           <a
             className="github-contributors__sponsors__logo github-contributors__sponsors__logo-placeholder"
             target="__blank"
@@ -70,7 +140,32 @@ const Contributors = () => {
       </div>
       <div className="justify-content-center text-center">
         <h3 className="github-contributors__thanks">
-          Thanks to all of our contributors!{" "}
+          All Sponsors&nbsp;
+          <span role="img" aria-label="appreciation">
+            ❤️
+          </span>
+        </h3>
+        <div className="github-contributors__sponsors">
+          {allSponsors.map((sponsor) => (
+            <a
+              key={sponsor.profile}
+              href={sponsor.profile}
+              title={sponsor.name}
+              target="_blank"
+              rel="noreferrer"
+              className="github-contributors__avatar"
+              style={{
+                backgroundImage: `url('${sponsor.image}')`,
+              }}
+            >
+              &nbsp;
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="justify-content-center text-center">
+        <h3 className="github-contributors__thanks">
+          Thanks to all of our contributors{" "}
           <span role="img" aria-label="appreciation">
             🎉👏
           </span>
