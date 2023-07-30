28f021d50c21b4bfc22d7fc4efc551d9b442d826
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-07-30 23:27:14 +0530

    Enabled analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
 import 'focus-visible';
+import {Analytics} from '@vercel/analytics/react';
+
 import '@/styles/tailwind.css';
 import '@/styles/carbon-ads.css';
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
