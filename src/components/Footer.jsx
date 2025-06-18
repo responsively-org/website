fa5703daadaeaef183ecfb369afaac9150954da4
lourduradjou <rajlourdu15@gmail.com>
2025-06-18 18:03:31 +0530

    fix: update non-JSX compliant attributes to valid JSX syntax

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -74,8 +74,8 @@ export function Footer() {
         </div>
       </Container>
       <Button className='fixed right-2 bottom-2 goToTop'  color="green" style={{width: '40px', height: '40px', padding: '4px 8px'}} onClick={goToTop}>
-        <svg xmlns="http://www.w3.org/2000/svg" width={'40px'} height={'40px'} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+        <svg xmlns="http://www.w3.org/2000/svg" width={'40px'} height={'40px'} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
         </svg>
       </Button>
     </footer>
